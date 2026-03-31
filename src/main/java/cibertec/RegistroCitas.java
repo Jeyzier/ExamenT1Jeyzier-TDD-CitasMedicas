61f0557de5388d9c77070ae7bb72507566eec6d2
Jeyzier <jeysev1899@gmail.com>
2026-03-30 19:43:53 -0500

    Test 3: Validar numero de documento

--- src/main/java/cibertec/RegistroCitas.java
+++ src/main/java/cibertec/RegistroCitas.java
@@ -9,14 +9,15 @@ public class RegistroCitas {
         if (codigo == null || !codigo.matches("^C\\d{3}$")) {
             return "Ingrese un código de cita válido";
         }
-
         String nombreSinEspacios = nombrePaciente != null ? nombrePaciente.replace(" ", "") : "";
 
-        // Validamos que solo tenga letras (y espacios) y que tenga al menos 5 letras
         if (nombrePaciente == null || !nombrePaciente.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$") || nombreSinEspacios.length() < 5) {
             return "El nombre del paciente debe tener al menos 5 caracteres alfabéticos";
         }
 
+        if (numeroDocumento == null || !numeroDocumento.matches("^\\d{8}$")) {
+            return "Ingrese un número de documento válido";
+        }
         return "Registro exitoso";
     }
 
--- src/test/java/cibertec/RegistroCitasTest.java
+++ src/test/java/cibertec/RegistroCitasTest.java
@@ -21,11 +21,25 @@ class RegistroCitasTest {
     @Test
     void testRegistrarCita_NombrePacienteInvalido() {
         RegistroCitas registro = new RegistroCitas();
+
         String resultado = registro.registrarCita("C123", "Ana1", "12345678", "2026-12-31");
         System.out.println("El mensaje devuelto fue: " + resultado);
         assertEquals("El nombre del paciente debe tener al menos 5 caracteres alfabéticos", resultado);
 
         String resultado1 = registro.registrarCita("C123", "Ana María", "12345678", "2026-12-31");
+        System.out.println("El mensaje devuelto fue: " + resultado1);
+        assertEquals("Registro exitoso", resultado1);
+    }
+
+    @Test
+    void testRegistrarCita_DocumentoInvalido() {
+        RegistroCitas registro = new RegistroCitas();
+
+        String resultado = registro.registrarCita("C123", "Maria Lopez", "12345A", "2026-12-31");
+        System.out.println("El mensaje devuelto fue: " + resultado);
+        assertEquals("Ingrese un número de documento válido", resultado);
+
+        String resultado1 = registro.registrarCita("C123", "Maria Lopez", "12345678", "2026-12-31");
         System.out.println("El mensaje devuelto fue: " + resultado1);
         assertEquals("Registro exitoso", resultado1);
     }
